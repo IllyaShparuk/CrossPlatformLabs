cefc7e8d39ff2a37b330c1c6aecdff3c069c4e53
IllyaShparuk <illya.shparuk@gmail.com>
2024-09-25 21:30:56 +0300

    task's core

---
+++ Lab1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Lab1
+{
+    public abstract class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(BinaryNumbersCount(121, 2));
+        }
+
+        private static int BinaryNumbersCount(int n, int k)
+        {
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                string binary = DecimalToBinary(i);
+                if (ZerosCount(binary, k))
+                    count++;
+            }
+            return count;
+        }
+
+        private static bool ZerosCount(string binary, int k)
+        {
+            int zeros = 0;
+            foreach (var bit in binary)
+                if (bit == '0')
+                    zeros++;
+            return zeros == k;
+        }
+
+        private static string DecimalToBinary(int number) => DecimalToBinaryHelper(number, "");
+
+        private static string DecimalToBinaryHelper(int number, string binary)
+        {
+            binary = (number % 2) + binary;
+            return number < 2 ? binary : DecimalToBinaryHelper(number / 2, binary);
+        }
+    }
+}
