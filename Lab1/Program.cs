847097e4dc8e2f7d6908d7d4ba1bc339b517f933
IllyaShparuk <illya.shparuk@gmail.com>
2024-10-01 12:36:54 +0300

    Input via File INPUT.txt

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -7,8 +7,35 @@ namespace Lab1
     {
         private static void Main()
         {
-            Console.WriteLine(BinaryNumbersCount(121, 2));
+            string? basePath = AppContext.BaseDirectory;
+            basePath = FindProjectDirectory(basePath);
+            if (basePath == null) throw new Exception("Could not find project directory");
+            string inputFilePath = Path.Combine(basePath, "INPUT.txt");
+            string outputFilePath = Path.Combine(basePath, "OUTPUT.txt");
+            string[] lines = File.ReadAllLines(inputFilePath);
+            if (lines == null) throw new Exception("Could not read input file or there is no INPUT.txt");
+            List<string> res = new List<string>();
+            foreach (string line in lines)
+            {
+                string[] numbers = line.Split(" ");
+                if (!int.TryParse(numbers[0], out int n) || !int.TryParse(numbers[1], out int k)) continue;
+                res.Add(BinaryNumbersCount(n, k).ToString());
+                Console.WriteLine($"n = {n}, k = {k}: {res.Last()}");
+            }
+
+            File.WriteAllLines(outputFilePath, res);
         }
+
+        private static string? FindProjectDirectory(string? currentDirectory)
+        {
+            while (currentDirectory != null && !Directory.GetFiles(currentDirectory, "*.csproj").Any())
+            {
+                currentDirectory = Directory.GetParent(currentDirectory)?.FullName;
+            }
+
+            return currentDirectory;
+        }
+
 
         private static int BinaryNumbersCount(int n, int k)
         {
@@ -19,6 +46,7 @@ namespace Lab1
                 if (ZerosCount(binary, k))
                     count++;
             }
+
             return count;
         }
 
