e9059cb383537ae2f82c536ba33609f2cded3bfe
IllyaShparuk <illya.shparuk@gmail.com>
2024-11-19 21:24:19 +0200

    ClassLibraryForLab4 + Lab 4 version and help commands

---
+++ ClassLibraryForLab4/Lab3.cs
@@ -0,0 +1,23 @@
+﻿namespace ClassLibraryForLab4;
+
+public static class Lab3
+{
+    public static void Start(string inputFilePath, string outputFilePath)
+    {
+        try
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            Console.WriteLine($"Read lines from file [\"{inputFilePath}\"]");
+
+            Labyrinth colorfulLabyrinth = new Labyrinth(lines);
+
+            string result = colorfulLabyrinth.GetResult();
+            File.WriteAllText(outputFilePath, result);
+            Console.WriteLine($"Result is written to output file [\"{outputFilePath}\"]: {result}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error message: " + e.Message);
+        }
+    }
+}
--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,3 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Reflection;
+using McMaster.Extensions.CommandLineUtils;
 
-Console.WriteLine("Hello, World!");
+namespace Lab4;
+
+[Subcommand(typeof(Run), typeof(Version), typeof(Help), typeof(SetPath))]
+public class Program
+{
+    public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+    private static string? ProjectVersion => Assembly.GetEntryAssembly()?.GetName().Version?.ToString();
+
+    private static string? Authors =>
+        Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyCompanyAttribute>()?.Company;
+
+
+    private void OnExecute() => CommandLineApplication.Execute<Program>("help");
+
+    [Command(Name="help")]
+    class Help
+    {
+        private void OnExecute()
+        {
+            Console.WriteLine("Commands to use:");
+            Console.WriteLine("\thelp".PadRight(60) + "Show usage guide");
+            Console.WriteLine("\tversion".PadRight(60) + "General information");
+            Console.WriteLine("\trun <LAB> [-i|--input <file>] [-o|--output <file>]".PadRight(60) +
+                              "Runs selected program");
+            Console.WriteLine("\tset-path -p|--project <path>".PadRight(60) + "Sets the path as environment variable");
+        }
+    }
+
+    [Command(Name = "run")]
+    class Run
+    {
+        // TODO
+        private void OnExecute() => Console.WriteLine("Hello World!");
+    }
+
+    [Command(Name = "version")]
+    class Version
+    {
+        private void OnExecute() =>
+            Console.WriteLine("\tApp version:".PadRight(20) + ProjectVersion + Environment.NewLine +
+                              "\tAuthor:".PadRight(20) + Authors);
+    }
+
+    [Command(Name = "set-path")]
+    class SetPath
+    {
+        // TODO
+    }
+}
